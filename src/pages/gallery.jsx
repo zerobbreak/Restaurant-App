9fd861b0a857216a2b76bb06add5c70df1853692
zerobbreak <uthacker244@outlook.com>
2024-09-12 15:10:48 +0200

    Updates

--- src/pages/gallery.jsx
+++ src/pages/gallery.jsx
@@ -1,14 +1,12 @@
 import { useState, useEffect } from "react";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
-import styles from "../style";
 
 const GalleryPage = () => {
-  const [showEvents, setShowEvents] = useState(true); // State to toggle between Events and Food
+  const [showEvents, setShowEvents] = useState(true); // Toggle between Events and Food
   const [currentEventImageIndex, setCurrentEventImageIndex] = useState(0);
   const [currentFoodImageIndex, setCurrentFoodImageIndex] = useState(0);
 
-  // Images for Events and Food
   const eventImages = [
     "/assets/events1.jpg",
     "/assets/events2.jpg",
@@ -32,21 +30,14 @@ const GalleryPage = () => {
   ];
 
   useEffect(() => {
-    // Event images loop
     const eventInterval = setInterval(() => {
-      setCurrentEventImageIndex(
-        (prevIndex) => (prevIndex + 1) % eventImages.length
-      );
+      setCurrentEventImageIndex((prevIndex) => (prevIndex + 1) % eventImages.length);
     }, 3000); // Change event image every 3 seconds
 
-    // Food images loop
     const foodInterval = setInterval(() => {
-      setCurrentFoodImageIndex(
-        (prevIndex) => (prevIndex + 1) % foodImages.length
-      );
+      setCurrentFoodImageIndex((prevIndex) => (prevIndex + 1) % foodImages.length);
     }, 4000); // Change food image every 4 seconds
 
-    // Cleanup intervals on component unmount
     return () => {
       clearInterval(eventInterval);
       clearInterval(foodInterval);
@@ -54,37 +45,35 @@ const GalleryPage = () => {
   }, [eventImages.length, foodImages.length]);
 
   return (
-    <div className="bg-white w-full overflow-hidden">
-      
-          <Navbar logoImage="/assets/logo2.svg" />
+    <div className="w-full min-h-screen overflow-hidden">
+      <Navbar logoImage="/assets/logo2.svg" />
 
       {/* Gallery Header */}
       <section className="px-6 py-16 bg-gray-50 text-gray-900">
         <div className="max-w-4xl mx-auto text-center">
-          <h1 className="text-5xl font-extrabold text-gray-800 mb-12">
+          <h1 className="text-6xl font-bold text-gray-900 mb-8">
             Gallery
           </h1>
-          <p className="text-lg text-gray-600">
-            Explore our gallery featuring events and delicious food at Just
-            Badela.
+          <p className="text-xl text-gray-500 mb-12">
+            Explore our gallery featuring events and delicious food at Just Badela.
           </p>
-          <div className="mb-12">
+          <div className="flex justify-center mb-12 space-x-4">
             <button
               onClick={() => setShowEvents(true)}
-              className={`px-6 py-3 mx-2 font-semibold rounded ${
+              className={`px-8 py-3 font-semibold rounded-full transition-all duration-300 ease-in-out shadow ${
                 showEvents
-                  ? "bg-primary text-white"
-                  : "bg-gray-200 text-gray-800"
+                  ? "bg-primary text-white hover:bg-primary-dark"
+                  : "bg-gray-200 text-gray-700 hover:bg-gray-300"
               }`}
             >
               Events
             </button>
             <button
               onClick={() => setShowEvents(false)}
-              className={`px-6 py-3 mx-2 font-semibold rounded ${
+              className={`px-8 py-3 font-semibold rounded-full transition-all duration-300 ease-in-out shadow ${
                 !showEvents
-                  ? "bg-primary text-white"
-                  : "bg-gray-200 text-gray-800"
+                  ? "bg-primary text-white hover:bg-primary-dark"
+                  : "bg-gray-200 text-gray-700 hover:bg-gray-300"
               }`}
             >
               Food
@@ -98,24 +87,22 @@ const GalleryPage = () => {
         <section
           className="px-6 py-16 bg-white text-gray-900 relative flex items-center justify-center"
           style={{
-            backgroundImage: `url('/assets/events1.jpg')`,
+            backgroundImage: `url(${eventImages[currentEventImageIndex]})`,
             backgroundSize: "cover",
             backgroundPosition: "center",
+            transition: "background-image 0.5s ease-in-out",
           }}
         >
-          <div className="absolute inset-0 bg-black opacity-50"></div>{" "}
-          {/* Dimming overlay */}
+          <div className="absolute inset-0 bg-black opacity-40"></div>
           <div className="max-w-6xl mx-auto relative z-10 text-center">
-            {" "}
-            {/* Ensures content is above the overlay and centered */}
-            <h2 className="text-4xl font-bold mb-8 text-white">Events</h2>
+            <h2 className="text-5xl font-semibold text-white mb-8">
+              Events
+            </h2>
             <div className="flex justify-center">
-              {" "}
-              {/* Center the image */}
               <img
                 src={eventImages[currentEventImageIndex]}
                 alt={`Event ${currentEventImageIndex + 1}`}
-                className="w-full max-w-md h-[500px] object-cover rounded-lg shadow-md"
+                className="w-full max-w-lg h-[500px] object-cover rounded-lg shadow-md transform transition-transform duration-300 hover:scale-105"
               />
             </div>
           </div>
@@ -125,33 +112,31 @@ const GalleryPage = () => {
       {/* Food Section */}
       {!showEvents && (
         <section
-          className="px-6 py-16 bg-gray-50 text-gray-900 relative flex items-center justify-center"
+          className="px-6 py-16 bg-gray-100 text-gray-900 relative flex items-center justify-center"
           style={{
-            backgroundImage: `url('/assets/hero-img.jpg')`,
+            backgroundImage: `url(${foodImages[currentFoodImageIndex]})`,
             backgroundSize: "cover",
             backgroundPosition: "center",
+            transition: "background-image 0.5s ease-in-out",
           }}
         >
-          <div className="absolute inset-0 bg-black opacity-50"></div>{" "}
-          {/* Dimming overlay */}
+          <div className="absolute inset-0 bg-black opacity-40"></div>
           <div className="max-w-6xl mx-auto relative z-10 text-center">
-            {" "}
-            {/* Ensures content is above the overlay and centered */}
-            <h2 className="text-4xl font-bold mb-8 text-white">Food</h2>
+            <h2 className="text-5xl font-semibold text-white mb-8">
+              Food
+            </h2>
             <div className="flex justify-center">
-              {" "}
-              {/* Center the image */}
               <img
                 src={foodImages[currentFoodImageIndex]}
                 alt={`Food ${currentFoodImageIndex + 1}`}
-                className="w-full max-w-md h-[500px] object-cover rounded-lg shadow-md"
+                className="w-full max-w-lg h-[500px] object-cover rounded-lg shadow-md transform transition-transform duration-300 hover:scale-105"
               />
             </div>
           </div>
         </section>
       )}
 
-      {/* Footer Section */}
+      {/* Footer */}
       <Footer />
     </div>
   );
